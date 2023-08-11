c5db3a318ad404e79e055898bc910e3b8b90274e
shoet <32211937+shoet@users.noreply.github.com>
2023-08-11 19:46:27 +0900

    コメント追加

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -3,6 +3,14 @@ import { signIn, signUp } from '@/services/auth'
 import { ApiResponse } from '@/type/api'
 import { BadRequest } from '@/type/error'
 
+/**
+ * 認証API（サインイン）
+ * @param req Request
+ * @param res Response
+ * @param next NextFunction
+ * @returns ユーザー、ステータスコード
+ *
+ */
 export const signInHandler = async (
   req: Request,
   _res: Response,
@@ -15,12 +23,21 @@ export const signInHandler = async (
     throw new BadRequest('password is require', req)
   }
   const user = await signIn(req.body.email, req.body.password)
+  // TODO: setCookie
   return {
     data: user,
     status: 200,
   }
 }
 
+/**
+ * 認証API（サインアップ）
+ * @param req Request
+ * @param res Response
+ * @param next NextFunction
+ * @returns ユーザー、ステータスコード
+ *
+ */
 export const signUpHandler = async (
   req: Request,
   _res: Response,
--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -3,6 +3,14 @@ import * as userService from '@/services/user'
 import { ApiResponse } from '@/type/api'
 import { BadRequest, NotFound } from '@/type/error'
 
+/**
+ * ユーザーAPI（個別取得）
+ * @param req Request
+ * @param res Response
+ * @param next NextFunction
+ * @returns ユーザー、ステータスコード
+ *
+ */
 export const getUserHandler = async (
   req: Request,
   _res: Response,
