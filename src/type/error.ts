5966b9e81b0d62d79897dcefc0e777da4b771d7d
shoet <32211937+shoet@users.noreply.github.com>
2023-08-11 11:05:33 +0900

    lint

--- src/type/error.ts
+++ src/type/error.ts
@@ -1,47 +1,47 @@
-import { Request } from "express";
+import { Request } from 'express'
 
 export class BadRequest extends Error {
-  status: number;
-  req: Request;
-  message: string;
+  status: number
+  req: Request
+  message: string
 
   constructor(message: string, req: Request) {
-    super("BadRequest");
-    this.status = 400;
-    this.req = req;
-    this.message = message;
+    super('BadRequest')
+    this.status = 400
+    this.req = req
+    this.message = message
   }
 }
 
 export class NotFound extends Error {
-  status: number;
-  message: string;
+  status: number
+  message: string
 
   constructor(message: string) {
-    super("NotFound");
-    this.status = 404;
-    this.message = message;
+    super('NotFound')
+    this.status = 404
+    this.message = message
   }
 }
 
 export class Unauthorized extends Error {
-  status: number;
-  message: string;
+  status: number
+  message: string
 
-  constructor(message: string) {
-    super("Unauthorized");
-    this.status = 401;
-    this.message = message;
+  constructor(message?: string) {
+    super('Unauthorized')
+    this.status = 401
+    this.message = message ?? 'Unauthorized'
   }
 }
 
 export class Conflict extends Error {
-  status: number;
-  message: string;
+  status: number
+  message: string
 
   constructor(message: string) {
-    super("Conflict");
-    this.status = 409;
-    this.message = message;
+    super('Conflict')
+    this.status = 409
+    this.message = message
   }
 }
